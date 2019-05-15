05db34c6a029e8781f173e10aca79580b5e7fd98
Neil Wilkinson <nmwilkinson@gmail.com>
2019-05-15 08:30:45 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:treasure_nfc/src/app.dart';
+
+void main() => runApp(App());
---
+++ lib/src/app.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'lifecycle/lifecycle_reactor.dart';
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: TextTheme(body1: TextStyle(fontSize: 18.0)),
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NFC Hunt"),
+      ),
+      body: AppLifecycleReactor(),
+    );
+  }
+}
